d8201b16b885a439ef8ade59845339aab08febbd
Duskim <duskimm@gmail.com>
2023-03-03 11:14:06 -0300

    01

---
+++ my-app/src/About.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+class About extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>Douglas</h1>
+        <p>asdasdasdasdasdasdsadfwfewf</p>
+        <h2>Minhas Habilidades</h2>
+        <ul>
+          <li>01</li>
+          <li>02</li>
+          <li>03</li>
+        </ul>
+      </>
+    )
+  }
+}
+
+export default About;
