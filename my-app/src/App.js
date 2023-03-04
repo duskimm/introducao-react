fa6045c258be3ca3d08a1c7ee36b8f27af07d782
Duskim <duskimm@gmail.com>
2023-03-04 08:12:12 -0300

    image

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -20,3 +20,19 @@ class App extends React.Component {
 }
 
 export default App;
+/*
+import React from 'react';
+import Image from './Image';
+
+function App() {     <============
+  
+  const URL = 'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg';
+  return (
+    <main>
+      <Image source={ URL } alternativeText="Cute cat staring" />
+    </main>
+  );
+}
+
+export default App;
+*/
